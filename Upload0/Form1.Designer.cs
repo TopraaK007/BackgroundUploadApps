bedeaae8f2012cef8677af6749517b10220c2e61
TopraaK007 <100555485+TopraaK007@users.noreply.github.com>
2023-08-30 18:29:31 +0300

    Proje dosyası ekle.

---
+++ Upload0/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Upload0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BackUpload = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BackUpload
+            // 
+            this.BackUpload.ContextMenuStrip = this.contextMenuStrip1;
+            this.BackUpload.Icon = ((System.Drawing.Icon)(resources.GetObject("BackUpload.Icon")));
+            this.BackUpload.Text = "BacgroundUploaded";
+            this.BackUpload.Visible = true;
+            this.BackUpload.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.BackUpload_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(94, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 81);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.VisibleChanged += new System.EventHandler(this.Form1_VisibleChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon BackUpload;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Upload0/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Upload0
+{
+
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public string Server { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string UploadTime { get; set; }
+        public string DocumentPath { get; set; }
+        public int WaitTime { get; set; }
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+            string xmlPath = "v2.xml";
+            await LoadXml(xmlPath);
+
+        }
+        private async Task LoadXml(string xml)
+        {
+
+            if (!File.Exists(xml))
+            {
+                MessageBox.Show("Dosya Bulunamadı");
+            }
+            else
+            {
+                try
+                {
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load(xml);
+                 
+                    XmlNode xmlNode = xmldoc.SelectSingleNode("/file");
+
+                    Server = xmlNode.SelectSingleNode("server").InnerText;
+                    Username = xmlNode.SelectSingleNode("username").InnerText;
+                    Password = xmlNode.SelectSingleNode("password").InnerText;
+                    DocumentPath = xmlNode.SelectSingleNode("document").InnerText;
+                    WaitTime = int.Parse(xmlNode.SelectSingleNode("time").InnerText);
+                    UploadTime = xmlNode.SelectSingleNode("lastupload").InnerText;
+
+                   
+
+                    //Username = Username.Substring(10);
+                    //Password = Password.Substring(10);
+                    await EditFiles();
+                }
+                catch (Exception ex)
+                {
+                    BackUpload.BalloonTipIcon = ToolTipIcon.Error;
+                    BackUpload.BalloonTipTitle = "Xml Hatası!";
+                    BackUpload.BalloonTipText = ex.Message.ToString();
+                    BackUpload.ShowBalloonTip(5000);
+                    
+                }
+
+            }
+
+        }
+
+        private DateTime uploadTime = DateTime.MinValue;
+        private async Task EditFiles()
+        {
+
+            while (true)
+            {
+
+                int waitTime = WaitTime;
+
+                BackUpload.Text = "Yükleniyor....";
+               
+
+                try
+                {
+                    string LocalFilePath = DocumentPath;
+                    string RemoteFile = "/";
+                    string[] files = Directory.GetFiles(LocalFilePath);
+                    using (WebClient client = new WebClient())
+                    {
+                        client.Credentials = new NetworkCredential(Username, Password);
+
+                        foreach (string file in files)
+                        {
+                            string fileName = Path.GetFileName(file);
+                            string remoteFilePath = RemoteFile + fileName;
+                            string ftpUrl = $"ftp://{Server}/{remoteFilePath}";
+
+                            client.UploadFile(ftpUrl, WebRequestMethods.Ftp.UploadFile, file);
+                        }
+                        uploadTime = DateTime.Now;
+                        SaveLastUploadTimeToXml(uploadTime);
+                        BackUpload.Text = "Yükeleme Tamam Bir Sonraki Bekleniyor";
+                        await Task.Delay(waitTime * 1000);
+                        
+                    }
+
+
+                }
+                catch (WebException ex)
+                {
+                    if (ex.Message.Contains("530"))
+                    {
+                        BackUpload.BalloonTipIcon = ToolTipIcon.Error;
+                        BackUpload.BalloonTipTitle = "Kullanıcı Adı veya Şifre Hatalı!";
+                        BackUpload.BalloonTipText = ex.Message.ToString();
+                        BackUpload.ShowBalloonTip(5000);
+                        BackUpload.Text = "Yükleme Yapılamadı!";
+                        break;
+                    }
+                    else
+                    {
+                        BackUpload.BalloonTipIcon = ToolTipIcon.Error;
+                        BackUpload.BalloonTipTitle = "Ftp Sunucu Hatası!";
+                        BackUpload.BalloonTipText = ex.Message.ToString();
+                        BackUpload.ShowBalloonTip(5000);
+                        BackUpload.Text = "Yükleme Yapılamadı!";
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        private void SaveLastUploadTimeToXml(DateTime lastuploadTime)
+        {
+         
+                string xmlPath = "v2.xml";
+
+                try
+                {
+                    XDocument xmlDocument = XDocument.Load(xmlPath);
+                    xmlDocument.Root.Element("lastupload").Value = lastuploadTime.ToString("dd.MM.yyyy HH:mm:ss");
+                    xmlDocument.Save(xmlPath);
+                }
+                catch (Exception ex)
+                {
+                    // XML kaydetme hatası oluştuğunda işlem yapılabilir
+                    MessageBox.Show("XML kaydetme hatası: " + ex.Message);
+                }
+            
+        }
+
+        
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void Form1_VisibleChanged(object sender, EventArgs e)
+        { 
+                this.Hide();
+                
+        }
+        private void BackUpload_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string xmlPath = "v2.xml";
+
+            if (File.Exists(xmlPath))
+            {
+                try
+                {
+                   XDocument document = XDocument.Load(xmlPath);
+
+
+                    XElement uploadTimeElement = document.Root.Element("lastupload");
+                    UploadTime= uploadTimeElement.Value;
+                    BackUpload.ShowBalloonTip(3000, "Bilgilendirme.", $"Son Yükleme{UploadTime}", ToolTipIcon.Info);
+                }
+                catch (Exception ex)
+                { 
+                    BackUpload.ShowBalloonTip(3000, $"Yükleme Yapılamadı!+{ex.ToString()}", $"Son Yükleme{UploadTime}", ToolTipIcon.Warning);
+                }
+            }
+        }
+    }
+}
